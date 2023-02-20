ccd6b34a709174d88c46961c353155249412a181
Shaka Suzy <shaheedchambers@gmail.com>
2023-02-20 12:10:50 -0500

    Added new comments to outline tasks and whats needed

--- assets/weatherScript.js
+++ assets/weatherScript.js
@@ -1,1 +1,18 @@
 //Global Variables
+//global variables
+//Function to get saved seaches out of local storage
+//function to get global postioning from city name and pull current weather
+//Store latitude and longitude for saved city search
+
+//Function to get current days weather for single card
+//APPEND!(City Name, Date, Weather Icons, Temperature)
+
+//Function to get current days weather for 5-day forcast
+//APPEND!(City Name, Date, Weather Icons, Temperature)
+//Day 1
+//Day 2
+//Day 3
+//Day 4
+//Day 5
+
+//event listener when clickes run Search fuction
