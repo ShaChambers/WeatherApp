f0cbb1346a56bb84733146e401e19009b6e57fe7
Shaka Suzy <shaheedchambers@gmail.com>
2023-02-20 12:59:25 -0500

    global vars added in script

--- assets/weatherScript.js
+++ assets/weatherScript.js
@@ -1,5 +1,16 @@
 //Global Variables
 //global variables
+var submitBtn = document.querySelector("#submit-btn");
+var cityInput = document.querySelector("#city-input");
+var savedSearchBtn = document.querySelector("#saved-searches");
+var currentWeatherCard = document.querySelector("#current-weather");
+var forecastCard = document.querySelector("#forecast-cards");
+var day1Card = document.querySelector("#day1forecast");
+var day2Card = document.querySelector("#day2forecast");
+var day3Card = document.querySelector("#day3forecast");
+var day4Card = document.querySelector("#day4forecast");
+var day5Card = document.querySelector("#day5forecast");
+var searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
 //Function to get saved seaches out of local storage
 //function to get global postioning from city name and pull current weather
 //Store latitude and longitude for saved city search
